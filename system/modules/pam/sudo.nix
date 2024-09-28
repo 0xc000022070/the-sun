0aa55c2414ee16a0713c82b802193aeae74830b3
Luis Quiñones <lpaandres2020@gmail.com>
2024-09-28 14:11:38 -0600

    feat(system): user 'luisnquin' can reboot and poweroff without being prompted about sudo password

--- system/modules/pam/sudo.nix
+++ system/modules/pam/sudo.nix
@@ -2,4 +2,21 @@
   security.sudo = {
     enable = true;
     wheelNeedsPassword = true;
+
+    extraRules = [
+      {
+        users = ["luisnquin"];
+        commands = [
+          {
+            command = "/run/current-system/sw/bin/poweroff";
+            options = ["NOPASSWD"];
+          }
+          {
+            command = "/run/current-system/sw/bin/reboot";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
 }
